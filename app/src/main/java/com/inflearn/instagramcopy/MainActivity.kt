8f6245f83f99c6a480de426df612dd77623e1e2b
김준기 <gugi329@daum.net>
2022-01-26 18:23:00 +0900

    유저 페이지 구현 (1)

--- app/src/main/java/com/inflearn/instagramcopy/MainActivity.kt
+++ app/src/main/java/com/inflearn/instagramcopy/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.inflearn.instagramcopy.navigation.AddPhotoActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -61,6 +62,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
             R.id.action_account -> {
                 var userFragment = UserFragment()
+                var bundle = Bundle()
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                bundle.putString("destinationUid", uid)
+                userFragment.arguments = bundle
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.main_content, userFragment).commit()
                 return true
