ce7a36417fb7a7e78fc51aa3936bff302d7fbf07
김준기 <gugi329@daum.net>
2022-01-26 16:56:21 +0900

    Firebase Authentication (2)  - Google

--- app/src/main/java/com/inflearn/instagramcopy/LoginActivity.kt
+++ app/src/main/java/com/inflearn/instagramcopy/LoginActivity.kt
@@ -4,13 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     var auth: FirebaseAuth? = null
+
+    var googleSignInClient: GoogleSignInClient? = null
+
+    var GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +31,48 @@ class LoginActivity : AppCompatActivity() {
         email_login_button.setOnClickListener {
             signInAndSignUp()
         }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("684435037531-2i97j2sqdfm179o2aaag7gp63nkdnkfv.apps.googleusercontent.com")
+            .requestEmail().build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        google_sign_in_button.setOnClickListener {
+//            First step
+            googleLogin()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+            var result = data?.let { Auth.GoogleSignInApi.getSignInResultFromIntent(it) }
+            if (result != null) {
+                if (result.isSuccess) {
+                    var account = result.signInAccount
+//                    Second step
+                    firebaseAuthWithGoogle(account)
+                }
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+//                    Login
+                moveMainInPage(task.result?.user)
+            } else {
+//                    Show the error message
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
     }
 
     fun signInAndSignUp() {
